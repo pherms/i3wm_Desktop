927d0e596652faadca3db73bd6f908c975f3143c
Pascal Herms <pherms@outlook.com>
2019-10-16 19:00:09 +0200

    running config

---
+++ pacsync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+updatefile=~/.config/i3/pacsync.info
+aurfile=~/.config/i3/aursync.info
+
+if [ -f $updatefile ]
+then
+	rm $updatefile
+fi
+
+if [ -f $aurfile ]
+then
+	rm $aurfile
+fi
+
+pacman -Quq | wc -l >> $updatefile
+cower -u | wc -l >> $aurfile
+
+echo "Pacman: "$(exec cat $updatefile)" | AUR: "$(exec cat $aurfile)
